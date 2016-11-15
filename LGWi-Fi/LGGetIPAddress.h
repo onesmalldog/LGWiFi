a8c811d3a7d55b7d6f899104921a1a4ca608460a
lee zg <598407521@qq.com>
2016-11-15 10:18:53 +0800

    Create LGGetIPAddress.h

---
+++ LGWi-Fi/LGGetIPAddress.h
@@ -0,0 +1,13 @@
+//
+//  LGGetIPAddress.h
+//  text_Wifi
+//
+//  Created by 东途 on 2016/11/3.
+//  Copyright © 2016年 displayten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LGGetIPAddress : NSObject
++ (NSString *)getIP;
+@end
