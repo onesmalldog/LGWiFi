c603c0afa8f6845c6f20093c5b43705122f3244b
lee zg <598407521@qq.com>
2016-11-15 10:17:01 +0800

    Update LGWifiManager.h

--- LGWi-Fi/LGWifiManager.h
+++ LGWi-Fi/LGWifiManager.h
@@ -10,10 +10,21 @@
 #import <CoreWLAN/CoreWLAN.h>
 
 #define LGWiFi [LGWifiManager lg_sharedManager]
+
+@protocol LGWifiManagerDelegate <NSObject>
+@optional
+- (void)lg_ssidDidChangeToSSID:(NSString *_Nullable)ssid;
+@end
+
 @interface LGWifiManager : NSObject
 + (instancetype _Nonnull)lg_sharedManager;
 - (NSSet <CWNetwork *>* _Nullable)findNetworks;
 - (NSSet <NSString *>* _Nullable)findNetworksToSSID;
 - (BOOL)connectWifiFromName:(NSString *_Nonnull)name password:(NSString *_Nullable)password;
-- (BOOL)connectWifiWithNetwork:(CWNetwork *_Nonnull)network password:(NSString *_Nullable)password ;
+- (BOOL)connectWifiWithNetwork:(CWNetwork *_Nonnull)network password:(NSString *_Nullable)password;
+
+@property (copy, readonly, nonatomic, getter=getCurrentSSID) NSString *_Nullable currentSSID;
+@property (copy, readonly, nonatomic) NSString *_Nullable errorDescription;
+@property (strong, readonly, nonatomic) NSError *_Nullable error;
+@property (weak, nonatomic) id<LGWifiManagerDelegate>_Nullable delegate;
 @end
